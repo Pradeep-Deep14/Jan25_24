36f04b4b5ee357db8296df3b4eba43b0c7ac9227
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-01-25 18:49:02 +0530

    first commit

---
+++ 2.py
@@ -0,0 +1,12 @@
+my_numbers=[1,2,3,4,5]
+
+some_numbers=[]
+for number in my_numbers:
+    if (number %2!=0 and number %3==0):
+        continue
+    some_numbers.append(number)
+
+print(some_numbers)
+
+#True continue
+#False append
